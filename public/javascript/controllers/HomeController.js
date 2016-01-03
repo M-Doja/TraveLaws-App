6313154eb466da92d13836dd5b734e988b11c343
M-Doja <micdoja@yahoo.com>
2016-01-03 11:03:09 -0800

    added modal to states when pressed

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -3,13 +3,16 @@
 	angular.module('app')
 	.controller('HomeController', HomeController);
 
-	function HomeController($state, HomeFactory) {
+	function HomeController($state, HomeFactory,$scope, $mdDialog, $mdMedia) {
 		var vm = this;
 		vm.title = 'Welcome to TraveLaws';
 		vm.showLaws = true;
 		vm.States = HomeFactory.States;
 		vm.TraveLaws = false;
 		vm.enterTraveLaws = true;
+// adding dialog
+		$scope.status = '  ';
+		$scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
 		vm.showTraveLaws = function() {
 			vm.TraveLaws = true;
@@ -21,7 +24,33 @@
 			vm.enterTraveLaws = true;
 		}
 
-
+		$scope.showAdvanced = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: HomeController,
+      templateUrl: 'views/tabDialog.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+	function DialogController($scope, $mdDialog) {
+	  $scope.hide = function() {
+	    $mdDialog.hide();
+	  };
+	  $scope.cancel = function() {
+	    $mdDialog.cancel();
+	  };
+	  $scope.answer = function(answer) {
+	    $mdDialog.hide(answer);
+	  };
+	}
 
 
 	}
