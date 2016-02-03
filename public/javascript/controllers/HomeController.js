91a5fedc7a11cc120bae1a5aed44802e9cd8ebb5
M-Doja <micdoja@yahoo.com>
2016-02-03 15:49:06 -0800

    added metatags

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -6,36 +6,13 @@
 	function HomeController($state, HomeFactory,$scope, $mdDialog, $mdMedia) {
 		var vm = this;
 		vm.title = 'TravState';
-		vm.showLaws = true;
 		vm.States = HomeFactory.States;
-		vm.TraveLaws = true;
-		vm.showCamping = false;
-		vm.showAbout = false;
-		// vm.enterTraveLaws = true;
 // adding dialog
 vm.announceClick = function(index) {
-	$mdDialog.show(
-		$mdDialog.alert('Hello World')
-
-	);
+	$mdDialog.show($mdDialog.alert('Hello World'));
 };
 
-		vm.showCamp = function() {
-			vm.TraveLaws = false;
-			vm.showCamping = true;
-			vm.showAbout = false
-		}
-		vm.showAboutPage = function() {
-			vm.TraveLaws = false;
-			vm.showAbout = true;
-			vm.showCamping = false
-		}
 
-		vm.Home = function() {
-			vm.TraveLaws = true;
-			vm.showCamping = false;
-			vm.showAbout = false;
-		}
 
 
 
