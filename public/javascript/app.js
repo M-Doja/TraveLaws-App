c48f428d8592e6d35b4789321e798dc97d3069a0
M-Doja <micdoja@yahoo.com>
2016-02-02 18:26:40 -0800

    redesign of site

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,6 +7,15 @@
 		$stateProvider.state('Home',{
 			url: '/',
 			templateUrl: 'views/home.html'
+		}).state('Laws',{
+			url: '/states_laws',
+			templateUrl: 'views/Laws.html'
+		}).state('Camping',{
+			url: '/camping',
+			templateUrl: 'views/Camp.html'
+		}).state('About',{
+			url: '/about',
+			templateUrl: 'views/About.html'
 		});
 		$urlRouterProvider.otherwise('/');
 	}
